7cb9e929cf722cb7679d265509666900f6b7ccc7
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2022-06-28 20:32:24 +0300

    Solve MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,23 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+
+const App = defineComponent({
+  data() {
+    return {
+      title: '',
+      checkedBtn: undefined,
+    };
+  },
+
+  watch: {
+    checkedBtn(newV) {
+      fetchMeetupById(newV)
+        .then((res) => (this.title = res.title))
+        .catch((err) => alert(err));
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
