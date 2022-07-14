494906d5a1479f2dbb9008de4061ac41dd89d736
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2022-07-14 22:24:18 +0300

    Solve 04-vue-cli/02-TheToaster

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <ui-toast v-for="[k, v] in toastsList" :key="k" :icon="v.icon" :mod="v.mod" :text="v.text" />
   </div>
 </template>
 
 <script>
 import UiIcon from './UiIcon.vue';
-
+import UiToast from './UiToast.vue';
 export default {
   name: 'TheToaster',
 
-  components: { UiIcon },
+  components: { UiIcon, UiToast },
+
+  data() {
+    return {
+      toastsList: new Map(),
+    };
+  },
+
+  methods: {
+    _handleToastsList(options, time = 5000) {
+      const id = Math.random().toString(36).substring(2, 12);
+      this.toastsList.set(id, options);
+      setTimeout(() => this.toastsList.delete(id), time);
+    },
+
+    success(text) {
+      this._handleToastsList({ text, mod: 'success', icon: 'check-circle' });
+    },
+
+    error(text) {
+      this._handleToastsList({ text, mod: 'error', icon: 'alert-circle' });
+    },
+  },
 };
 </script>
 
@@ -39,35 +53,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
